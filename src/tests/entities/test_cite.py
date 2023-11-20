57d3d4d74180e1baa21f5aa4000724d2ca4ab59e
tuome <tonni@tuta.io>
2023-11-20 19:22:31 +0200

    Refactor with black

--- src/tests/entities/test_cite.py
+++ src/tests/entities/test_cite.py
@@ -5,10 +5,14 @@ from entities.cite import Cite
 
 class TestCite(unittest.TestCase):
     def setUp(self):
-        self.cite = Cite("amazing article", "article", {"year": 3202, "author": "Someone"})
+        self.cite = Cite(
+            "amazing article", "article", {"year": 3202, "author": "Someone"}
+        )
 
     def test_cite_can_be_initialized(self):
         Cite("amazing article", "article", {"year": 3202, "author": "Someone"})
 
     def test_cite_str(self):
-        self.assertEqual(str(self.cite), "amazing article - {'year': 3202, 'author': 'Someone'}")
+        self.assertEqual(
+            str(self.cite), "amazing article - {'year': 3202, 'author': 'Someone'}"
+        )
--- src/tests/services/test_logic.py
+++ src/tests/services/test_logic.py
@@ -8,5 +8,7 @@ class TestCite(unittest.TestCase):
         self.logic = Logic()
 
     def test_create_cite(self):
-        create = self.logic.create_cite("book", "Test", {"name":"Test1", "author":"Test Test", "year":1900 })
+        create = self.logic.create_cite(
+            "book", "Test", {"name": "Test1", "author": "Test Test", "year": 1900}
+        )
         self.assertEqual(create, "Cite added")
