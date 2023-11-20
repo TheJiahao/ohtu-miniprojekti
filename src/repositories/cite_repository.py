d6d17c55442957c7e20b2a2ce04cca36c905f57e
tuome <tonni@tuta.io>
2023-11-20 11:49:30 +0200

    Add initial cite_repository.py

---
+++ src/repositories/cite_repository.py
@@ -0,0 +1,15 @@
+from src.entities.cite import Cite
+
+
+class CiteRepository:
+    def __init__(self, db) -> None:
+        # self.db = db
+        pass
+
+    def add_cite(self, cite: Cite) -> None:
+        # db.add_cite(cite)
+        pass
+
+    def get_all_cites(self) -> list[Cite]:
+        # return self.db.get_all_cites()
+        pass
