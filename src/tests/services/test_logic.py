10aae77d562ce2a10ad0465c7de676414e9121cc
alannesanni <sanni.alanne@helsinki.fi>
2023-11-22 17:17:07 +0200

    Add logic test

--- src/tests/services/test_logic.py
+++ src/tests/services/test_logic.py
@@ -1,14 +1,19 @@
 import unittest
 from services.logic import Logic
-from entities.cite import Cite
+from unittest.mock import Mock
 
 
 class TestCite(unittest.TestCase):
     def setUp(self):
-        self.logic = Logic()
+        self.repository_mock = Mock()
+        self.logic = Logic(self.repository_mock)
 
     def test_create_cite(self):
-        create = self.logic.create_cite(
-            "book", "Test", {"name": "Test1", "author": "Test Test", "year": 1900}
-        )
+        self.repository_mock.add_cite()
+        create = self.logic.create_cite("book", 123, {"year": 1900, "author": "Hello"})
         self.assertEqual(create, "Cite added")
+
+    def test_create_cite_error(self):
+        self.repository_mock.add_cite.side_effect = Exception()
+        create = self.logic.create_cite("book", 123, {"year": 1900, "author": "Hello"})
+        self.assertEqual(create, "Error")
