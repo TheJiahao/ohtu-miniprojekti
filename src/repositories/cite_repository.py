82c29d0c159a5d0b5c27e8fc119a4d9e07d0269c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-24 15:39:07 +0200

    Refactor add_cite

--- src/repositories/cite_repository.py
+++ src/repositories/cite_repository.py
@@ -1,18 +1,19 @@
 from entities.cite import Cite
-from infrastructure.database import database
+from infrastructure.database import Database
+from infrastructure.database import database as default_database
 
 
-class CiteRepository:  # pylint: disable=too-few-public-methods
-    """Luokka vastaa Cite olion toimenpiteistä"""
+class CiteRepository:
+    """Luokka, joka vastaa Cite olion tallennuksesta"""
 
-    def __init__(self, db=database) -> None:
+    def __init__(self, database=default_database) -> None:
         """Luokan konstruktori
 
         Args:
             db (SQLiteDB, optional): SQLiteDB olio, oletuksena database.py:n sisällä luotu
         """
 
-        self.db = db
+        self._database: Database = database
 
     def add_cite(self, cite: Cite) -> None:
         """Kutsuu tietokannan add_cite() funktiota
@@ -21,16 +22,34 @@ class CiteRepository:  # pylint: disable=too-few-public-methods
             cite (Cite): Cite olio
         """
 
-        self.db.add_cite(cite)
+        self._database.cursor.execute(
+            """
+            INSERT INTO Cites (id, type) VALUES (?, ?)
+            """,
+            (cite.id, cite.type),
+        )
+
+        for name, content in cite.fields:
+            self._database.cursor.execute(
+                """
+                INSERT INTO Fields (cite_id, name, content) VALUES (?, ?, ?)
+                """,
+                (cite.id, name, content),
+            )
+
+        self._database.connection.commit()
 
     # def get_all_cites(self) -> list[Cite]:
     #     return self.db.get_all_cites()
     #     pass
 
     def get_cites(self):
-        cites = self.db.get_cites()
-        return cites
+        pass
+
+    def remove_all_cites(self) -> None:
+        """Poistaa kaikki viitteet."""
+
+        self._database.initialize()
 
 
-# muissa moduuleissa käytettävä olio
 cite_repository = CiteRepository()
