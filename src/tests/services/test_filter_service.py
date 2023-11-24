2e24ada318c4d11c1f2de582040d7fd9969cdf15
tuome <tonni@tuta.io>
2023-11-24 06:20:04 +0200

    Add FilterService __init__() test

---
+++ src/tests/services/test_filter_service.py
@@ -0,0 +1,10 @@
+import unittest
+from services.filter_service import FilterService
+
+
+class TestFilterService(unittest.TestCase):
+    def setUp(self) -> None:
+        self.filter_service = FilterService()
+
+    def test_init(self):
+        self.assertIsNotNone(self.filter_service)
