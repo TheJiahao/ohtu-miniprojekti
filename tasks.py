57be6ec75bbff1246be96899d77b729f65a2164c
tuome <tonni@tuta.io>
2023-11-25 07:27:02 +0200

    Add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,19 @@
+from invoke import task
+
+@task
+def main(ctx):
+    ctx.run("python3 src/main.py", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src && coverage html", pty=True)
+
+@task
+def refactor(ctx):
+    ctx.run("black src && pylint src", pty=True)
