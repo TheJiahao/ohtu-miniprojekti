f13843d99f4df0c798bb90f8e6ad9589f27bb512
Essi Savonius <essi.savonius@hotmail.fi>
2023-11-26 16:20:20 +0200

    Add StubCiteRepository for testing

---
+++ src/repositories/stub_cite_repository.py
@@ -0,0 +1,26 @@
+from entities.cite import Cite
+from infrastructure.database import Database
+from infrastructure.database import database as default_database
+
+
+class StubCiteRepository:
+    """Testausta varten luokka, joka vastaa Cite olion tallennuksesta"""
+
+    def __init__(self, cites: list,  database: Database = default_database) -> None:
+        """Luokan konstruktori
+
+        Args:
+            cites (list): syötetty lista viitteitä, eli Cites olioita
+            database (Database, optional): tietokanta
+        """
+
+        self._database: Database = database
+        self.cites = cites
+
+    def get_all_cites(self) -> list[Cite]:
+        """Hakee kaikki viitteet, tässä testiluokassa annettu jo parametrina
+
+        Returns:
+            list[Cite]: lista Cite olioita
+        """
+        return self.cites
