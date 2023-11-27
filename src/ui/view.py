9e032fc31889045fa6b309850c9171e1a7f02af1
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-27 10:58:35 +0200

    Add View class

---
+++ src/ui/view.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+from services.logic import Logic
+from infrastructure.console_io import ConsoleIO
+from entities.cite import Cite
+
+
+class View(ABC):
+    """Luokka, joka vastaa yksittäisestä näkymästä.
+
+    Args:
+        ABC (_type_): _description_
+    """
+
+    def __init__(
+        self, description: str, help_message: str, logic: Logic, io: ConsoleIO
+    ) -> None:
+        self.description: str = description
+        self._logic: Logic = logic
+        self._help_message: str = help_message
+        self._io: ConsoleIO = io
+
+    @abstractmethod
+    def start(self):
+        """Käynnistää näkymän."""
+        self._show_help()
+
+    def _show_help(self) -> None:
+        self._io.write(self._help_message)
+
+    def show_cites(self, cites: list[Cite]):
+        """Tulostaa viitteet.
+
+        Args:
+            cites (list[Cite]): Tulostettavat viitteet.
+        """
