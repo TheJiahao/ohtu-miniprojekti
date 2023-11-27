92425e7e8730415bb5d4ba3189ccef090fe185b4
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-27 11:54:35 +0200

    Refactore AddCiteView

--- src/ui/add_cite_view.py
+++ src/ui/add_cite_view.py
@@ -7,41 +7,39 @@ class AddCiteView(View):
     """Luokka, joka vastaa viitteen lisäysnäkymästä."""
 
     def __init__(self, logic: Logic, io: ConsoleIO) -> None:
-        help_message = """
-        Valitse viitetyyppi:
-        1: kirja (book)
-        2: artikkeli (article)
-        """
+        self._types: dict[int, str] = {1: "book", 2: "article"}
+
+        help_message = "\n".join(
+            [
+                "Valitse viitetyyppi:",
+                "1: kirja (book)",
+                "2: artikkeli (article)",
+            ]
+        )
 
         super().__init__("Viitteen lisäys", help_message, logic, io)
 
     def start(self) -> None:
         """Käynnistää viitteen lisäysnäkymän."""
-        super().start()
 
         fields = {}
 
-        print("\nValitse viitetyyppi:\n")
-        print("1: kirja (book)")
-        print("2: artikkeli (article)\n")
-        type = int(self._io.read())
+        super().start()
 
-        print("\nSyötä viitteen nimi: \n")
+        type = self._types[int(self._io.read())]
+
+        self._io.write("Syötä viitteen nimi: \n")
         id = str(self._io.read())
 
         # Seuraavaksi tulisi tyypistä riippuen eri kenttien kyselyitä
 
-        print("\nSyötä kirjailijat (authors), erota pilkulla: \n")
+        self._io.write("Syötä kirjailijat (authors), erota pilkulla: \n")
         authors = (self._io.read()).split(", ")
 
-        print("\nSyötä otsikko (title): \n")
-        fields["title"] = str(self._io.read())
+        self._io.write("Syötä otsikko (title): \n")
+        fields["title"] = self._io.read()
 
-        print("\nSyötä vuosi (year): \n")
-        fields["year"] = int(self._io.read())
+        self._io.write("Syötä vuosi (year): \n")
+        fields["year"] = self._io.read()
 
         self._logic.create_cite(id, type, authors, fields)
-
-
-view = AddCiteView(Logic(), ConsoleIO())
-view.start()
