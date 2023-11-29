4a62e2964950f708fbc5d4e430665e4c28d52c28
tuome <tonni@tuta.io>
2023-11-29 10:57:15 +0200

    Implement FilterCiteView functionality

--- src/ui/filter_cite_view.py
+++ src/ui/filter_cite_view.py
@@ -1,3 +1,5 @@
+from infrastructure.console_io import ConsoleIO
+from services.logic import Logic
 from ui.view import View
 
 
@@ -7,3 +9,31 @@ class FilterCiteView(View):
     Args:
         View (_type_): _description_
     """
+
+    def __init__(self, logic: Logic, io: ConsoleIO) -> None:
+        self._filtertypes: dict[int, str] = {1: "name", 2: "author", 3: "tag"}
+
+        help_message = "\n".join(
+            ["Valitse hakutyyppi:", "1: nimi", "2: kirjailija", "3: tunniste"]
+        )
+        super().__init__("Viitteiden haku", help_message, logic, io)
+
+    def start(self) -> None:
+        """Käynnistää hakunäkymän"""
+
+        super().start()
+
+        type = self._filtertypes[int(self._io.read())]
+
+        match type:
+            case "name":
+                self._io.write("Syötä viitteen nimi: \n")
+            case "author":
+                self._io.write("Syötä kirjailija: \n")
+            case "tag":
+                self._io.write("Syötä tunniste: \n")
+
+        filters = {type}
+        search = str(self._io.read())
+
+        super().show_cites(self._logic.filter_cites(search, filters))
