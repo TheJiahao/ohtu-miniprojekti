14698d2827f6d931dfb9cbc1e1eb8de6d3102544
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-29 19:14:50 +0200

    Update test task

--- tasks.py
+++ tasks.py
@@ -9,16 +9,14 @@ def main(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
+    ctx.run("robot src/tests/robot", pty=True)
 
 
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src && coverage html", pty=True)
 
+
 @task
 def refactor(ctx):
     ctx.run("black src && pylint src", pty=True)
-
-@task
-def robot(ctx):
-    ctx.run("robot src/tests/robot", pty=True)
