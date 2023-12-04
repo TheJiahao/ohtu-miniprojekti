5a9ca966bbb15a0311c7dd74f612be9de93d8c40
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-04 11:39:50 +0200

    Add dump_cite to BibtexExporter

--- src/infrastructure/bibtex_exporter.py
+++ src/infrastructure/bibtex_exporter.py
@@ -3,6 +3,32 @@ from entities.cite import Cite
 
 class BibtexExporter:
     @classmethod
+    def dump_cite(cls, cite: Cite) -> str:
+        """Palauttaa viitteen BibTeX-muodossa kentät järjestettynä.
+
+        Args:
+            cite (Cite): Muunnettava viite.
+
+        Returns:
+            str: Viite BibTeX-muodossa.
+        """
+
+        lines = [f"@{cite.type}{{{cite.id}"]
+
+        fields = [f"author = {cls.dump_list(cite.authors)}"]
+        fields.extend(
+            [
+                f"{name} = {cls.dump_string(content)}"
+                for name, content in cite.fields.items()
+            ]
+        )
+
+        lines.extend(sorted(fields))
+        lines.append("}")
+
+        return ",\n".join(lines)
+
+    @classmethod
     def dump_list(cls, data: list[str]) -> str:
         """Palauttaa listan BibTeX-kentän arvona ja järjestettynä.
 
--- src/tests/infrastructure/test_bibtex_exporter.py
+++ src/tests/infrastructure/test_bibtex_exporter.py
@@ -5,6 +5,66 @@ from entities.cite import Cite
 
 
 class TestBibtexExporter(unittest.TestCase):
+    def setUp(self):
+        self.acm_comminication = Cite(
+            "weiser1993some",
+            "article",
+            ["Weiser Mark"],
+            {
+                "journal": "Communications of the ACM",
+                "volume": "36",
+                "year": "1993",
+                "publisher": "ACM New York, NY, USA",
+            },
+        )
+
+        self.generalized_integral = Cite(
+            "mcleod1980generalized",
+            "book",
+            ["McLeod Robert M"],
+            {
+                "title": "The generalized Riemann integral",
+                "volume": "20",
+                "year": "1980",
+                "publisher": "American Mathematical Soc.",
+            },
+        )
+
+        self.relativity_theory = Cite(
+            "einstein1922general",
+            "incollection",
+            ["Albert Einstein"],
+            {
+                "title": "The general theory of relativity",
+                "booktitle": "The Meaning of Relativity",
+                "pages": "54--75",
+                "year": "1922",
+                "publisher": "Springer",
+            },
+        )
+
+        self.new_general_relativity = Cite(
+            "hayashi1979new",
+            "article",
+            ["Hayashi Kenji", "Shirafuji Takeshi"],
+            {
+                "title": "New general relativity",
+                "journal": "Physical Review D",
+                "volume": "19",
+                "number": "12",
+                "pages": "3524",
+                "year": "1979",
+                "publisher": "APS",
+            },
+        )
+
+        self.cites = [
+            self.acm_comminication,
+            self.generalized_integral,
+            self.relativity_theory,
+            self.new_general_relativity,
+        ]
+
     def test_dump_empty_list(self):
         result = BibtexExporter.dump_list([])
 
@@ -24,3 +84,16 @@ class TestBibtexExporter(unittest.TestCase):
 
     def test_dump_empty_string(self):
         self.assertEqual(BibtexExporter.dump_string(""), "{}")
+
+    def test_dump_cite(self):
+        expected = """@article{weiser1993some,
+author = {Weiser Mark},
+journal = {Communications of the ACM},
+publisher = {ACM New York, NY, USA},
+volume = {36},
+year = {1993},
+}"""
+
+        result = BibtexExporter.dump_cite(self.acm_comminication)
+
+        self.assertEqual(result, expected)
