c9460e856fa19115b769a520f577a6dc0248b81f
Ronja Tuovinen <ronja.tuovinen@helsinki.fi>
2023-12-06 16:22:27 +0200

    Id haku toimii

--- src/services/filter_service.py
+++ src/services/filter_service.py
@@ -55,12 +55,9 @@ class FilterService:
         Returns:
             list[Cite]: lista viiteolioita, jotka sopivat hakusanaan
         """
-
         cites = self.repository.get_all_cites()
-        if int(id) < 1:
-            return cites
-        filtered_cites = [cite for cite in cites if id in list(cite.id)]
+        filtered_cites = [cite for cite in cites if id in cite.id]
         return filtered_cites
 
     # def filter_by_tag(tag: str) -> list[Cite]:
-    #     pass
+    #     pass list(cite.id)
