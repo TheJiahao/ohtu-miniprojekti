4ee68f83fa7d11aa3b52d1845e059fdb5cbc99bb
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-10 15:49:43 +0200

    Implement ExportService

---
+++ src/services/export_service.py
@@ -0,0 +1,40 @@
+from entities.cite import Cite
+from infrastructure.bibtex_exporter import BibtexExporter
+
+
+class ExportService:
+    """
+    Luokka, joka vastaa viitteiden tiedostoon viemisestä.
+    """
+
+    def __init__(self, exporters: dict | None = None) -> None:
+        """
+        Luokan konstruktori.
+
+        Args:
+            exporters (dict | None, optional): Viitteiden viemisestä vastaavat oliot.
+            Oletukseltaan None.
+        """
+
+        self.__exporters: dict = exporters or {"bibtex": BibtexExporter()}
+
+    def export(self, path: str, format: str, cites: list[Cite]) -> None:
+        """
+        Kirjoittaa viitteet annettuun tiedostoon.
+
+        Args:
+            path (str): Polku kirjoitettavaan tiedostoon.
+            format (str): Tiedostomuoto. Tuetut muodot: bibtex
+            cites (list[Cite]): Kirjoitettavat viitteet.
+
+        Raises:
+            ValueError: Tiedostomuoto ei ole tuettu.
+        """
+
+        try:
+            self.__exporters[format].export(path, cites)
+
+        except KeyError as exception:
+            raise ValueError(
+                f"{format}-tiedostoon vieminen ei ole tuettu."
+            ) from exception
---
+++ src/tests/services/test_export_service.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+from unittest.mock import Mock
+
+from config import DATA_DIRECTORY
+from services.export_service import ExportService
+from entities.cite import Cite
+
+
+class TestExportService(unittest.TestCase):
+    def setUp(self):
+        self.exporters = {"bibtex": Mock(), "json": Mock(), "toml": Mock()}
+        self.export_service = ExportService(self.exporters)
+        self.path = os.path.join(DATA_DIRECTORY, "test", "exported_file")
+        self.cites = [Cite("cite1", "book"), Cite("cite2", "article")]
+
+    def test_export_with_unsupported_format_raises_exception(self):
+        with self.assertRaises(ValueError):
+            self.export_service.export(self.path, "yaml", self.cites)
+
+    def test_export_with_valid_path_and_format(self):
+        self.export_service.export(self.path, "bibtex", self.cites)
+
+    def test_export_calls_export_method_of_exporter(self):
+        self.export_service.export(self.path, "bibtex", self.cites)
+
+        self.exporters["bibtex"].export.assert_called_once_with(self.path, self.cites)
